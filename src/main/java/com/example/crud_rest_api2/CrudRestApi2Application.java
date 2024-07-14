9b40a108730059eafeda1509955f9b5ffa94477f
ebielle <eryblongo@gmail.com>
2024-07-14 16:43:19 +0200

    exercise

---
+++ src/main/java/com/example/crud_rest_api2/CrudRestApi2Application.java
@@ -0,0 +1,13 @@
+package com.example.crud_rest_api2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudRestApi2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudRestApi2Application.class, args);
+	}
+
+}
